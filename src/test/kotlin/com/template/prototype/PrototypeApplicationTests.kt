2bd813678067e546f8e644d607d00813b181d062
jayking <mojunseo49@naver.com>
2024-01-10 03:34:39 +0900

    git init

---
+++ src/test/kotlin/com/template/prototype/PrototypeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.template.prototype
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
